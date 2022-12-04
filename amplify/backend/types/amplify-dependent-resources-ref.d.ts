0d0dd088737119ee9c895c86486836e7840dbebd
sambulo senda <sambulosenda@sambulos-MBP.broadband>
2022-12-04 18:07:27 +0000

    fix

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -22,7 +22,6 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "api": {
         "Photogram": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
